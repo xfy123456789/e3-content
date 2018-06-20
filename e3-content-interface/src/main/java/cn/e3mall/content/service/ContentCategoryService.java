5e29bf84a3ef47dff32ebd0503c0f31590f755e8
xfy1233 <x983796531@163.com>
2018-06-20 18:06:00 +0800

    1

---
+++ e3-content-interface/src/main/java/cn/e3mall/content/service/ContentCategoryService.java
@@ -0,0 +1,9 @@
+package cn.e3mall.content.service;
+
+import java.util.List;
+
+import cn.e3mall.common.pojo.EasyUITreeNode;
+
+public interface ContentCategoryService {
+	List<EasyUITreeNode>getContentCatList(long parentId);
+}
---
+++ e3-content-service/src/main/java/cn/e3mall/content/service/impl/ContentCategoryServiceImpl.java
@@ -0,0 +1,45 @@
+package cn.e3mall.content.service.impl;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cn.e3mall.common.pojo.EasyUITreeNode;
+import cn.e3mall.content.service.ContentCategoryService;
+import cn.e3mall.mapper.TbContentCategoryMapper;
+import cn.e3mall.pojo.TbContentCategory;
+import cn.e3mall.pojo.TbContentCategoryExample;
+import cn.e3mall.pojo.TbContentCategoryExample.Criteria;
+/**
+ * 内容分类管理service
+ * <p>Title: ContentCategoryServiceImpl</p>
+ * <p>Description: </p>
+ * <p>Company: www.itcast.cn</p> 
+ * @version 1.0
+ */
+@Service
+public class ContentCategoryServiceImpl implements ContentCategoryService {
+
+	@Autowired
+	private TbContentCategoryMapper contentCategoryMapper;
+	@Override
+	public List<EasyUITreeNode> getContentCatList(long parentId) {
+		TbContentCategoryExample example=new TbContentCategoryExample();
+		Criteria criteria= example.createCriteria();
+		criteria.andParentIdEqualTo(parentId);
+	    List<TbContentCategory> catList= contentCategoryMapper.selectByExample(example);
+	    List<EasyUITreeNode> nodeList=new ArrayList<>();
+	    for (TbContentCategory tbContentCategory : catList) {
+			EasyUITreeNode node=new EasyUITreeNode();
+			node.setId(tbContentCategory.getId());
+			node.setText(tbContentCategory.getName());
+			node.setState(tbContentCategory.getIsParent()?"closed":"open");
+			nodeList.add(node);
+		}
+		return nodeList;
+	}
+
+}
